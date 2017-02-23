28f8e9c9cf89eb371e780382346b51d2e57e2f2a
Red-F0x <ho11owman.wf@gmail.com>
2017-02-23 23:09:22 +0100

    initial commit

---
+++ include/acf/ref/bits/preprocessor.hpp
@@ -0,0 +1,59 @@
+/*
+ * preprocessor.hpp
+ *
+ *  Created on: 09.02.2017
+ *      Author: ho11owman
+ */
+
+#ifndef PREPROCESSOR_HPP_
+#define PREPROCESSOR_HPP_
+
+//http://en.cppreference.com/w/cpp/preprocessor
+#include "acf/std_type_traits.h"
+
+#define ACF_REF_NAME(Type)                          \
+namespace acf {                                     \
+                                                    \
+namespace ref {                                     \
+                                                    \
+template <>                                         \
+    struct name<Type>                               \
+    {                                               \
+        static constexpr char value[] = #Type;      \
+        using type = decltype(value);               \
+    };                                              \
+                                                    \
+}                                                   \
+                                                    \
+}
+
+#define ACF_REF_INDEX(Type, tt_index)               \
+namespace acf {                                     \
+                                                    \
+namespace ref {                                     \
+                                                    \
+template <>                                         \
+struct index<Type, tt_index> :                      \
+    public acf::ref::index_impl<Type, tt_index> {}; \
+                                                    \
+}                                                   \
+                                                    \
+}
+
+#define ACF_REF_MEM_NAME(Type, tt_index, tt_name)   \
+namespace acf {                                     \
+                                                    \
+namespace ref {                                     \
+                                                    \
+template <>                                         \
+    struct name<acf::ref::index<Type, tt_index>>    \
+    {                                               \
+        static constexpr char value[] = #tt_name;   \
+        using type = decltype(value);               \
+    };                                              \
+                                                    \
+}                                                   \
+                                                    \
+}
+
+#endif /* PREPROCESSOR_HPP_ */
