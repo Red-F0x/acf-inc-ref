9ba558c2c1e86692e5dd44d434fb3563246ee4cd
Wassili Funk <ho11owman.wf@gmail.com>
2017-03-17 09:06:24 +0100

    switch implementation details to macro definition

--- include/ref/bits/name_of_impl.h
+++ include/ref/bits/name_of_impl.h
@@ -11,189 +11,39 @@
 #include "ref/bits/fwd.h"
 #include "ref/bits/preprocessor.h"
 
-REGISTER_REF_NAME_OF(void);
-namespace ref {
+/// define name_of for void type
+REGISTER_REF_NAME_OF_AS(void,                   "void");
 
-//template <>
-//struct name_of<void>
-//{
-//    static constexpr char value[] { "void" };
-//    using value_type = decltype(value);
-//};
-//
-//constexpr char name_of<void>::value[];
+/// define name_of for std::nullptr_t type
+REGISTER_REF_NAME_OF_AS(std::nullptr_t,        "nullptr");
 
-template <>
-struct name_of<decltype(nullptr)>
-{
-    static constexpr char value[] = { "nullptr" };
-    using type = decltype(value);
-};
+/// define name_of for bool type
+REGISTER_REF_NAME_OF_AS(bool,                   "bool");
 
-constexpr char name_of<decltype(nullptr)>::value[];
+/// define name_of for character types
+REGISTER_REF_NAME_OF_AS(signed char,            "signed char");
+REGISTER_REF_NAME_OF_AS(unsigned char,          "unsigned char");
+REGISTER_REF_NAME_OF_AS(char,                   "char");
+REGISTER_REF_NAME_OF_AS(wchar_t,                "wchar_t");
+REGISTER_REF_NAME_OF_AS(char16_t,               "char16_t");
+REGISTER_REF_NAME_OF_AS(char32_t,               "char32_t");
 
-template <>
-struct name_of<bool>
-{
-    static constexpr char value[] = { "bool" };
-    using type = decltype(value);
-};
+/// define name_of for integer types
+REGISTER_REF_NAME_OF_AS(short int,              "short int");
+REGISTER_REF_NAME_OF_AS(unsigned short int,     "unsigned short int");
 
-constexpr char name_of<bool>::value[];
+REGISTER_REF_NAME_OF_AS(int,                    "int");
+REGISTER_REF_NAME_OF_AS(unsigned int,           "unsigned int");
 
-template <>
-struct name_of<signed char>
-{
-    static constexpr char value[] = { "signed char" };
-    using type = decltype(value);
-};
+REGISTER_REF_NAME_OF_AS(long int,               "long int");
+REGISTER_REF_NAME_OF_AS(unsigned long int,      "unsigned long int");
 
-constexpr char name_of<signed char>::value[];
+REGISTER_REF_NAME_OF_AS(long long int,          "long long int");
+REGISTER_REF_NAME_OF_AS(unsigned long long int, "unsigned long long int");
 
-template <>
-struct name_of<unsigned char>
-{
-    static constexpr char value[] = { "unsigned char" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<unsigned char>::value[];
-
-template <>
-struct name_of<char>
-{
-    static constexpr char value[] = { "char" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<char>::value[];
-
-template <>
-struct name_of<wchar_t>
-{
-    static constexpr char value[] = { "wchar_t" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<wchar_t>::value[];
-
-template <>
-struct name_of<char16_t>
-{
-    static constexpr char value[] = { "char16_t" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<char16_t>::value[];
-
-template <>
-struct name_of<char32_t>
-{
-    static constexpr char value[] = { "char32_t" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<char32_t>::value[];
-
-template <>
-struct name_of<short int>
-{
-    static constexpr char value[] = { "short int" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<short int>::value[];
-
-template <>
-struct name_of<unsigned short int>
-{
-    static constexpr char value[] = { "unsigned short int" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<unsigned short int>::value[];
-
-template <>
-struct name_of<int>
-{
-    static constexpr char value[] = { "int" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<int>::value[];
-
-template <>
-struct name_of<unsigned int>
-{
-    static constexpr char value[] = { "unsigned int" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<unsigned int>::value[];
-
-template <>
-struct name_of<long int>
-{
-    static constexpr char value[] = { "long int" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<long int>::value[];
-
-template <>
-struct name_of<unsigned long int>
-{
-    static constexpr char value[] = { "unsigned long int" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<unsigned long int>::value[];
-
-template <>
-struct name_of<long long int>
-{
-    static constexpr char value[] = { "long long int" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<long long int>::value[];
-
-template <>
-struct name_of<unsigned long long int>
-{
-    static constexpr char value[] = { "unsigned long long int" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<unsigned long long int>::value[];
-
-template <>
-struct name_of<float>
-{
-    static constexpr char value[] = { "float" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<float>::value[];
-
-template <>
-struct name_of<double>
-{
-    static constexpr char value[] = { "double" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<double>::value[];
-
-template <>
-struct name_of<long double>
-{
-    static constexpr char value[] = { "long double" };
-    using type = decltype(value);
-};
-
-constexpr char name_of<long double>::value[];
-
-}
+/// define name_of for floating point types
+REGISTER_REF_NAME_OF_AS(float,                  "float");
+REGISTER_REF_NAME_OF_AS(double,                 "double");
+REGISTER_REF_NAME_OF_AS(long double,            "long double");
 
 #endif /* NAME_OF_IMPL_H_ */
